2b12058d67a741c260a7b266836dfa52eefb5ba2
Abdelkader belhadj <gaddour04@gmail.com>
2021-01-01 14:29:48 +0100

    update models

--- university/models.py
+++ university/models.py
@@ -14,8 +14,9 @@ class Formation(models.Model):
 class University(models.Model):
 	name=models.CharField(max_length=1000)
 	description=models.TextField()
-	formation=models.ManyToManyField(Formation,null=True,blank=True)
-	image=models.ImageField()
+	#formation=models.ManyToManyField(Formation,null=True,blank=True)
+	#image=models.ImageField()
+	image=models.TextField()
 	url=models.TextField(max_length=1000)
 
 
